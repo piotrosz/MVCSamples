49a9f0e62e0b419fc6d678c75110ffa37c14f420
Piotr Ludwiczuk <piotrosz@gmail.com>
2012-03-05 23:17:07 +0100

    Simple injection IValueCalculator, LinqValueCalculator

---
+++ NinjectExample1/IValueCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NinjectExample1
+{
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(params Product[] products);
+    }
+}
---
+++ NinjectExample1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Ninject;
+
+namespace NinjectExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardKernel kernel = new StandardKernel();
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+
+            IValueCalculator valueCalc = kernel.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(valueCalc);
+            Console.WriteLine("Total is: {0}", cart.CalculateStockValue());
+        }
+    }
+}
