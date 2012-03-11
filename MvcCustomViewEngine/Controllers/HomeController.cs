d609b5d92a634ba49d6c197f0cfbadeb716365c3
Piotr Ludwiczuk <piotrosz@gmail.com>
2012-03-11 10:28:36 +0100

    Added custom view engine implementation

---
+++ MvcCustomViewEngine/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomViewEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Hello MVC";
+            ViewData["Time"] = DateTime.Now.ToShortTimeString();
+
+            return View("DebugData");
+        }
+
+    }
+}
---
+++ MvcCustomViewEngine/Views/Infrastructure/CustomViewEngine/DebugDataViewEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomViewEngine.Views.Infrastructure.CustomViewEngine
+{
+    public class DebugDataViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            if (viewName == "DebugData") // hard-coded
+            {
+                return new ViewEngineResult(new DebugDataView(), this);
+            }
+            else
+            {
+                return new ViewEngineResult(new string[] { "Debug Data View Engine" });
+            }
+        }
+
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            return new ViewEngineResult(new string[] { "Debug Data View Engine" });
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            // do nothing
+        }
+    }
+}
